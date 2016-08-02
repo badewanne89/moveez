5dc57a1a3709f608f3825639a185d561bbdd003d
schdief <schdief.law@gmail.com>
2016-08-02 20:03:28 +0200

    Initial commit from local git

---
+++ moveez/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  moveez
+//
+//  Created by Steve Lohr on 02.08.16.
+//  Copyright © 2016 Steve Lohr. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
