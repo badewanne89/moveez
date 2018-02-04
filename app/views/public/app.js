b03873eacf57d9db1d4f042565ef5009dde9dd98
schdief <schdief.law@gmail.com>
2018-02-04 17:32:48 +0100

    added fadeout for flash messages

--- app/views/public/app.js
+++ app/views/public/app.js
@@ -1,11 +1,17 @@
 //dialogue to update a name
 function prepareModal(name, id) {
     //set name of modal
-    $('#modalOldName').text(name);
+    $('#modalOldName').text(name)
     //set placeholder
-    $('#modalInput').val(name);
+    $('#modalInput').val(name)
     //set action to id of title
     $('#modalForm').attr('action', '/title/' + id + '/?_method=PUT')
     //show modal
-    $('.ui.modal').modal('show');
+    $('.ui.modal').modal('show')
+}
+
+//trigger fadeout of flash messages
+window.onload = function() {
+    $('.success.message').fadeOut(2500)
+    $('.error.message').fadeOut(2500)
 }
